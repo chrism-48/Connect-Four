1b42404fb5df60755dd8182e54b5c57f92cd77f9
chrism-48 <moore4020@gmail.com>
2022-05-05 03:48:20 -0400

    adding js to move chips

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
-let redChip = document.getElementById('red');
-let blackChip = document.getElementById('black');
+// let redChip = document.getElementById('red');
+// let blackChip = document.getElementById('black');
 
-redChip.addEventListener('pointerdown', getRedCoordinates);
-blackChip.addEventListener('pointerdown', getBlackCoordinates);
+// redChip.addEventListener('pointerdown', getRedCoordinates);
+// blackChip.addEventListener('pointerdown', getBlackCoordinates);
 
 
-function getRedCoordinates() {
-    let redPosition = redChip.getBoundingClientRect();
-    let redX = Math.round(redPosition.left);
-    let redY = Math.round(redPosition.top);
-    console.log(`redX coordinates = ${redX}`);
-    console.log(`redY coordinates = ${redY}`);
-}
+// function getRedCoordinates() {
+//     let redPosition = redChip.getBoundingClientRect();
+//     let redX = Math.round(redPosition.left);
+//     let redY = Math.round(redPosition.top);
+//     console.log(`redX coordinates = ${redX}`);
+//     console.log(`redY coordinates = ${redY}`);
+// }
 
-function getBlackCoordinates() {
-    let blackPosition = blackChip.getBoundingClientRect();
-    let blackX = Math.round(blackPosition.left);
-    let blackY = Math.round(blackPosition.top);
-    console.log(`blackX coordinates = ${blackX}`);
-    console.log(`blackY coordinates = ${blackY}`);
-}
+// function getBlackCoordinates() {
+//     let blackPosition = blackChip.getBoundingClientRect();
+//     let blackX = Math.round(blackPosition.left);
+//     let blackY = Math.round(blackPosition.top);
+//     console.log(`blackX coordinates = ${blackX}`);
+//     console.log(`blackY coordinates = ${blackY}`);
+// }
+
+
+const myGameContainer = document.querySelector('.game-container');
+myGameContainer.addEventListener('pointerdown', userPressed, { passive: true });
+
+let element, bbox, startX, startY;
+
+function userPressed(event) {
+  element = event.target;
+  if (element.classList.contains('chips')) {
+    startX = event.clientX;
+    startY = event.clientY;
+    bbox = element.getBoundingClientRect();
+    myGameContainer.addEventListener('pointermove', userMoved, { passive: true });
+    myGameContainer.addEventListener('pointerup', userReleased, { passive: true });
+    myGameContainer.addEventListener('pointercancel', userReleased, { passive: true });
+  };
+};
+
+function userMoved(event) {
+  let deltaX = event.clientX - startX;
+  let deltaY = event.clientY - startY;
+  element.style.left = bbox.left + deltaX + "px";
+  element.style.top = bbox.top + deltaY + "px";
+};
+
+function userReleased(event) {
+  myGameContainer.removeEventListener('pointermove', userMoved);
+  myGameContainer.removeEventListener('pointerup', userReleased);
+  myGameContainer.removeEventListener('pointercancel', userReleased);
+};
+
